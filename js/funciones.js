cabb7ba937d5489b7503f92a713695bbaefcbe4b
Javier Martín <100529062+jotaeme890@users.noreply.github.com>
2022-12-03 16:28:00 +0100

    Add files via upload

--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,7 @@
 /*******************************************************************/
 /*       Web del proyecto  - JavaScript página principal           */
 /*           Autor : Javier Miguel Martín Gallardo                 */
-/*                 Versión : 1.0 - 06/11/2022                      */
+/*                 Versión : 1.0 - 01/12/2022                      */
 /*******************************************************************/
 
 
@@ -51,3 +51,70 @@ window.addEventListener("click", (e)=>{
 		})
 	}
 })
+
+/*************************  Modales  *******************************/
+var modaltor = document.getElementById("modtor");
+
+var btntor = document.getElementById("Btntor");
+
+var spantor = document.getElementById("closetor");
+
+btntor.onclick = function() {
+	modaltor.style.display = "block";
+}
+
+spantor.onclick = function() {
+	modaltor.style.display = "none";
+}
+
+window.onclick = function(event) {
+	if (event.target == modaltor) {
+    modaltor.style.display = "none";
+	}
+}
+
+
+var modalgal = document.getElementById("modgal");
+
+var btngal = document.getElementById("Btngal");
+
+var spangal = document.getElementById("closegal");
+
+btngal.onclick = function() {
+	modalgal.style.display = "block";
+}
+
+spangal.onclick = function() {
+	modalgal.style.display = "none";
+}
+
+window.onclick = function(event) {
+	if (event.target == modalgal) {
+		modalgal.style.display = "none";
+	}
+}
+
+/*************************  Galería  ******************************/
+var elements = document.getElementsByClassName("column");
+var i;
+
+function one() {
+	for (i = 0; i < elements.length; i++) {
+	elements[i].style.msFlex = "100%";  // IE10
+	elements[i].style.flex = "100%";
+}
+}
+
+function two() {
+	for (i = 0; i < elements.length; i++) {
+		elements[i].style.msFlex = "50%";  // IE10
+		elements[i].style.flex = "50%";
+	}
+}
+
+function four() {
+	for (i = 0; i < elements.length; i++) {
+		elements[i].style.msFlex = "25%";  // IE10
+		elements[i].style.flex = "25%";
+	}
+}
