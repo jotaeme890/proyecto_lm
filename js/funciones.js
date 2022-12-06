b07022cb7deb94e13ec036231356640c20d4958a
Javier Martín <100529062+jotaeme890@users.noreply.github.com>
2022-12-06 11:30:17 +0100

    Cambiada en inglés

--- js/funciones.js
+++ js/funciones.js
@@ -116,26 +116,3 @@ window.onclick = function(event) {
 }
 
 /*************************  Galería  ******************************/
-var elements = document.getElementsByClassName("column");
-var i;
-
-function one() {
-	for (i = 0; i < elements.length; i++) {
-	elements[i].style.msFlex = "100%";  // IE10
-	elements[i].style.flex = "100%";
-}
-}
-
-function two() {
-	for (i = 0; i < elements.length; i++) {
-		elements[i].style.msFlex = "50%";  // IE10
-		elements[i].style.flex = "50%";
-	}
-}
-
-function four() {
-	for (i = 0; i < elements.length; i++) {
-		elements[i].style.msFlex = "25%";  // IE10
-		elements[i].style.flex = "25%";
-	}
-}
